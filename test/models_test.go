25420ff9a1eb2967ec8a0a296f2ed39614047fc8
Hassaan Ali Wattoo <hassaanaliw@gmail.com>
2018-06-10 04:01:13 +0500

    Initial Commit, Setup CI

--- test/models_test.go
+++ test/models_test.go
@@ -10,3 +10,10 @@ func TestCreateCategory(t *testing.T) {
 	category := model.Category{Name: "Hello",}
 	assert.Equal(t, category.Name, "Hello", "Category name is not set correctly")
 }
+
+func TestCreateNote(t *testing.T) {
+	note := model.Note{
+		Title: "Test Note",
+	}
+	assert.Equal(t, note.Title, "Test Note", "Test Note not set correctly")
+}
